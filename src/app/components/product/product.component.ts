623e56502c6549e76d94e9820adb1aa9ac3416a1
unknown <you@example.com>
2020-10-08 00:19:37 +0300

    multi forms

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-product',
@@ -13,16 +14,82 @@ export class ProductComponent implements OnInit {
   submitted: boolean = false;
 
   constructor(private fb: FormBuilder) { 
-    this.productForm = this.fb.group({
-      email: ['', Validators.required],
-      phone: ['', Validators.required]
-    });
+    
   }
 
   ngOnInit(): void {
+    this.buildForm();
+  }
+
+  buildForm() {
+    this.productForm = this.fb.group({
+      email: [this.product.email, 
+        this.conditionalEmailValidator()
+        // this.conditionalValidator(
+        //   (() => this.product.email),
+        //   [
+        //     Validators.required
+            
+        //   ]
+        // )
+      ],
+
+      phone: [this.product.phone, 
+        this.conditionalPhoneValidator()
+        // this.conditionalValidator(
+        //   (() => this.product.phone),
+        //   Validators.required
+        // )
+      ]
+    });
+  }
+
+  // conditionalValidator(condition: (() => boolean), validator: ValidatorFn): ValidatorFn {
+  //   return (control: AbstractControl): {[key: string]: any} => {
+  //     if (! condition()) {
+  //       return null;
+  //     }
+  //     return validator(control);
+  //   }
+  // }
+
+  conditionalEmailValidator(){
+    if(this.product.email) {
+      return [
+          Validators.required, 
+          Validators.maxLength(250),
+          Validators.minLength(5),
+          Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")
+        ]
+    }
+    return null;
+  }
+
+  conditionalPhoneValidator(){
+    if(this.product.phone) {
+      return [
+          Validators.required, 
+          Validators.maxLength(12),
+          Validators.minLength(9),
+          // Validators.pattern("^[0-9]*$")
+          // Validators.pattern("^[0][2|3|4|8|9]{1}[-]{0,1}[0-9]{7}$"),
+          Validators.pattern("^[0][5][0|2|3|4|5|9]{1}[-]{0,1}[0-9]{7}$"),
+          // Validators.pattern("^[0][7][3|7]{1}[-]{0,1}[0-9]{7}$")
+        ]
+    }
+    return null;
+  }
+  
+  get email(){
+    return this.productForm.get('email');
+  }
+
+  get phone(){
+    return this.productForm.get('phone');
   }
 
   onFormSubmit() {
     this.submitted = true;
+    console.log("form valid: ", this.productForm.valid);
   }
 }
